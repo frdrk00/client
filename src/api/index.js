58e808334210a1f5189ee89a601f8d735a776a09
frdrk00 <masterkooo@gmail.com>
2023-05-20 13:51:06 +0200

    upgrade baseURL

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const baseURL = "http://127.0.0.1:5001/food-app-frdrk00/us-central1/app"
+export const baseURL = "https://us-central1-food-app-frdrk00.cloudfunctions.net/app"
 
 export const validateUserJWTToken = async (token) => {
     try {
